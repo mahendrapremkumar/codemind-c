5255c66d5a2fb1b441c84582c39d769f43a4c95c
mahendrapremkumar <147476330+mahendrapremkumar@users.noreply.github.com>
2023-12-21 20:19:39 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     int arr[a];     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     for(j=a-1;j>=0;j--)     {         if(arr[j]%2!=0)         {             printf("%d",j);             break;         }     } }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    int arr[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(j=a-1;j>=0;j--)
+    {
+        if(arr[j]%2!=0)
+        {
+            printf("%d",j);
+            break;
+        }
+    }
+}
